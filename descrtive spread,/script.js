68c5b0651f89ac27d8ce831d57b02a5c7ccc908d
kamalnathdhekwar <123158118+kamalnathdhekwar@users.noreply.github.com>
2023-05-08 17:55:28 +0530

    Add files via upload

---
+++ descrtive spread,/script.js
@@ -0,0 +1,16 @@
+const oneArray = [1,2,3,4]
+const twoArray = [5,6,7,8]
+
+// const threeArray = oneArray.concat(twoArray)
+
+const threeArray = [...oneArray,...twoArray]
+// console.log(threeArray);
+// ...  ye dots , spread opertors hai
+
+// +++++++++++++++++++++
+
+function manyArguments()
+{
+    let args = Array.from(arguments)
+    args.fo
+}
